caf54602ac70f27a8481e7fd12a9caf53fb32c7d
EarthNatthanan <92196787+EarthNatthanan@users.noreply.github.com>
2022-01-01 17:48:03 +0700

    Update index.js
    INDEX.JS

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ client.db = require("quick.db");
 client.commands = new Discord.Collection();
 client.cooldown = new Discord.Collection();
 client.config = {
-    TOKEN: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", //Discord Bot Token
-    API_TOKEN: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", //API Token found at http://api.snowflakedev.cf:9019/dashboard
+    TOKEN: "Your bot token", //Discord Bot Token
+    API_TOKEN: "API TOKEN", //API Token found at http://api.snowflakedev.cf:9019/dashboard
     prefix: "!",
     cooldown: 15000
 };
